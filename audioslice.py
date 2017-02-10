c99bcc2f47ec8c7aecd021caab0a299dc018144d
Andrew Carpenter <acarp52@gmail.com>
2017-02-10 11:41:37 -0500

    Added most textgrids

--- audioslice.py
+++ audioslice.py
@@ -1,13 +1,24 @@
+import os
 from pydub import AudioSegment
 
-sound = AudioSegment.from_mp3("./Dialects/northern-english49.mp3")
 
-# len() and slicing are in milliseconds
-halfway_point = len(sound) / 2
-second_half = sound[halfway_point:]
+def main():
 
-# Concatenation is just adding
-second_half_3_times = second_half + second_half + second_half
+    for filename in os.listdir("Dialects/mp3"):
+        sound = AudioSegment.from_mp3("Dialects/mp3/{0}".format(filename))
 
-# writing mp3 files is a one liner
-second_half_3_times.export("/path/to/new/file.mp3", format="mp3")
+        # len() and slicing are in milliseconds
+        halfway_point = len(sound) / 2
+        timeSlice = len(sound) / 20
+        slices = 1
+        startTime = 0
+        endTime = timeSlice
+
+        while slices <= 20:
+            newSlice = sound[startTime:endTime]
+            newSlice.export("Dialects/mp3/{0}_{1}.mp3".format(filename, slices), format="mp3")
+            startTime = endTime
+            endTime = endTime + timeSlice
+            slices += 1
+
+main()
