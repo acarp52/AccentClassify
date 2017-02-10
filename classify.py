66bd0bf54ffee1164dc2d335c92ecffb444dd5f1
Sushant Kafle <sushantkafle@Sushants-MacBook-Pro.local>
2017-02-09 20:33:03 -0500

    Upload (.wav) files and transcript

--- classify.py
+++ classify.py
@@ -1,7 +1,11 @@
 import re
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import cross_val_score
+from sklearn.cross_validation import cross_val_score
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.feature_selection import RFECV
 from sklearn import metrics
 import sys
 
@@ -30,10 +34,34 @@ npdata = np.array(data)
 print("You have ", npdata.shape[0], "training instanaces")
 print("You have ", npdata.shape[1], "features")
 
+
 ## very, very basic classification with Naive Bayes classifier
 gnb = GaussianNB()
 scores = cross_val_score(gnb, npdata, nptarget, cv=5, scoring='f1')
 print("Baseline classification F1:", np.average(scores))
+
+svc = SVC(kernel="linear")
+rfecv = RFECV(estimator=svc, step=1, cv=5, scoring='f1')
+selector = rfecv.fit(npdata, nptarget)
+
+selected_features = []
+npdata_sel = np.zeros((npdata.shape[0], selector.n_features_))
+
+index = 0
+for i, rank in enumerate(selector.ranking_):
+	if rank == 1:
+		selected_features.append(labels[i])
+		npdata_sel[:, index] = npdata[:, i]
+		index += 1
+
+print("Selected Features (%s)" %(','.join(selected_features)))
+print("New feature dimension (%d)" % npdata_sel.shape[1])
+
+scores = cross_val_score(svc, npdata_sel, nptarget, cv=5, scoring='f1')
+print("SVM (linear) classification F1 (with feature selection):", np.average(scores))
+
+scores = cross_val_score(SVC(kernel='rbf'), npdata, nptarget, cv=5, scoring='f1')
+print("SVM (rbf) classification F1 (without feature selection):", np.average(scores))
 
 ## Let's say you only want to use features 3 and 4...
 #npdata = npdata[:,1:3]
